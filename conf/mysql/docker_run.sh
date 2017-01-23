799c275e70bab5510c32855e4ce74e94eed17a0d
zhangxiaozheng <zhangxiaozheng@yidian-inc.com>
2017-01-23 19:53:12 +0800

    add source to mysql run.sh

--- conf/mysql/docker_run.sh
+++ conf/mysql/docker_run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+source ../../bin/setting.conf
 docker run --detach \
 	--hostname mysql.tigerson.com \
 	--name $CONTAINER_NAME \
 	--publish 3306:3306 \
 	--env TZ=Asia/Shanghai \
 	--env LANG=en_US.UTF-8 \
-	-v /Users/admin/myDockers/datas/mysql/data:/var/lib/mysql \
-	-v /Users/admin/myDockers/datas/mysql/custom:/etc/mysql/conf.d \
+	-v $DATA_BASE_DIR/mysql/data:/var/lib/mysql \
+	-v $DATA_BASE_DIR/mysql/custom:/etc/mysql/conf.d \
 	-e MYSQL_ROOT_PASSWORD=qwer1234 \
 	$IMAGE_NAME
